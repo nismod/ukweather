07b9088af5a1d2867920751dfb9da6a8cb88bc72
Lahiru Jayasuriya <Jayasuriyalt@cardiff.ac.uk>
2019-04-22 19:48:07 +0100

    read data and set main vars

--- weather_module/run.py
+++ weather_module/run.py
@@ -10,3 +10,33 @@ Outline :
 8. Write data to input_paramter and then input_timestep tables.
 
 """
+import os
+import numpy as np 
+import pandas as pd 
+
+hist_year = 2010
+current_timestep = 2020 #how to get this ?
+scenario = 1
+
+def read_data(hist_year, current_timestep, scenario):
+       
+    #path to files
+    hist_data =  'C:\\Users\Lahiru Jayasuriya\\Desktop\\NISMOD\\nismod2\\data\\energy_supply\\scenarios'
+    weather_data = 'C:\\Users\\Lahiru Jayasuriya\\Desktop\\NISMOD\\nismod2\\data\\scenarios\\climate'
+
+    #files
+    file_wind = "wss__NF"+str(scenario)+".csv"
+    file_solar = "rsds__NF"+str(scenario)+".csv"
+    file_historic = "weather_hist_hourly_"+str(hist_year)+".csv"
+
+    weather_hist = pd.read_csv(hist_data+os.path.sep+file_historic)
+    wind_daily = pd.read_csv(weather_data+os.path.sep+file_wind)
+    solar_daily = pd.read_csv(weather_data+os.path.sep+file_solar)
+
+    return (weather_hist, wind_daily, solar_daily)
+
+def daily_mean_hist(weather_hist):
+
+    wind_hist = weather_hist[weather_hist.parameter == 'wind']
+    solar_hist = weather_hist[weather_hist.parameter == 'insolation']
+            
